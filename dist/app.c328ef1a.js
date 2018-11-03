8f13c7a33468df6b6423f67111392126c1efab0a
shahab Uddin <shahabuddinirfan@gmail.com>
2018-11-03 18:10:28 +0500

    Converted to redux

--- dist/app.c328ef1a.js
+++ dist/app.c328ef1a.js
@@ -39800,7 +39800,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "35873" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "33813" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
